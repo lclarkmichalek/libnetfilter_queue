124cee777b8bd874db7f11af41bbe46bfd33ea2e
Laurie Clark-Michalek <lclarkmichalek@gmail.com>
2015-04-15 12:09:33 +0100

    Fix NFQError box errors

--- src/nfq.rs
+++ src/nfq.rs
@@ -23,39 +23,39 @@ enum ErrorReason {
 pub struct NFQError {
     reason: ErrorReason,
     description: String,
-    cause: Option<Error>,
+    cause: Option<Box<Error>>,
 }
 
 impl fmt::Debug for NFQError {
     fn fmt(&self, formatter: &mut fmt::Formatter) -> Result<(), fmt::Error> {
         let msg = format!("{:?}: {:?} (cause: {:?})",
                           self.reason, self.description, self.cause);
-        formatter.write_str(msg.as_slice())
+        formatter.write_str(msg.as_ref())
     }
 }
 
 impl fmt::Display for NFQError {
     fn fmt(&self, formatter: &mut fmt::Formatter) -> Result<(), fmt::Error> {
         let msg = format!("{:?} ({:?})", self.reason, self.description);
-        formatter.write_str(msg.as_slice())
+        formatter.write_str(msg.as_ref())
     }
 }
 
 impl Error for NFQError {
     fn description(&self) -> &str {
-        self.description.as_slice()
+        self.description.as_ref()
     }
     fn cause(&self) -> Option<&Error> {
         self.cause.as_ref().map(|c| &**c)
     }
 }
 
-impl<E: Error> From<E> for NFQError {
+impl<E: Error + 'static> From<E> for NFQError {
     fn from(err: E) -> NFQError {
         NFQError {
             reason: ErrorReason::Unknown,
             description: err.description().to_string(),
-            cause: Some(err),
+            cause: Some(Box::new(err)),
         }
     }
 }
@@ -170,6 +170,18 @@ struct CallbackData<A> {
     func: fn(ctx: &mut A, msg: &NFGenMsg, ad: &NFQData) -> c_int
 }
 
+enum NFQCopyMode {
+    NONE = 0,
+    META = 1,
+    PACKET = 2
+}
+
+pub enum CopyMode {
+    None,
+    Metadata,
+    Packet(u16)
+}
+
 #[link(name="netfilter_queue")]
 extern {
     fn nfq_create_queue(handle: *mut nfq_handle,
@@ -180,6 +192,21 @@ extern {
                                           data: *mut c_void) -> c_int,
                         data: *mut c_void) -> *mut nfq_q_handle;
     fn nfq_destroy_queue(handle: *mut nfq_q_handle) -> c_int;
+
+    fn nfq_fd(handle: *mut nfq_handle) -> c_int;
+    fn nfq_handle_packet(handle: *mut nfq_handle,
+                         buf: *mut u8,
+                         len: c_int) -> c_int;
+    fn nfq_set_mode(handle: *mut nfq_q_handle,
+                    mode: uint8_t,
+                    range: uint32_t) -> c_int;
+    fn nfq_set_queue_maxlen(handle: *mut nfq_q_handle,
+                            queuelen: uint32_t) -> c_int;
+    fn nfq_set_verdict(handle: *mut nfq_q_handle,
+                       id: uint32_t,
+                       verdict: uint32_t,
+                       data_len: uint32_t,
+                       buf: *const u8) -> c_int;
 }
 
 pub struct NFQQueue<A> {
@@ -212,9 +239,6 @@ impl<A> NFQQueue<A> {
                ) -> Result<NFQQueue<A>, NFQError> {
         let _g = NFQ_LOCK.lock().unwrap();
 
-
-
-
         let fpointer: *const nfq_q_handle = null();
         let apointer: *const A = null();
         // So we initialise the queue obj as empty apart from the ctx
@@ -238,10 +262,38 @@ impl<A> NFQQueue<A> {
                                             queue_callback::<A>,
                                             mem::transmute(&queue.cb_data)) };
         if ptr.is_null() {
-            Err(error("Failed to create queue"))
+            Err(error(ErrorReason::CreateQueue, "Failed to create queue", None))
         } else {
             queue.ptr = ptr;
             Ok(queue)
+        }
+    }
+
+    pub fn mode(&mut self, mode: CopyMode) -> Result<(), NFQError> {
+        let cmode = match mode {
+            CopyMode::None => NFQCopyMode::NONE,
+            CopyMode::Metadata => NFQCopyMode::META,
+            CopyMode::Packet(_) => NFQCopyMode::PACKET
+        } as uint8_t;
+        let range = match mode {
+            CopyMode::Packet(r) => r,
+            _ => 0
+        } as uint16_t;
+
+        let res = unsafe { nfq_set_mode(self.ptr, mode, range) };
+        if res != 0 {
+            Err(error(ErrorReason::SetQueueMode, "Failed to set queue mode", Some(res)))
+        } else {
+            Ok(())
+        }
+    }
+
+    pub fn queue_maxlen(&mut self, len: u32) -> Result<(), NFQError> {
+        let res = unsafe { nfq_set_queue_maxlen(self.ptr, len as uint32_t) };
+        if res != 0 {
+            Err(error(ErrorReason::SetQueueMaxlen, "Failed to set queue maxlen", Some(res)))
+        } else {
+            Ok(())
         }
     }
 }
