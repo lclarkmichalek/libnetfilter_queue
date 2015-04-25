ab2d7e0169a3df5d03caa6cc1aeff27ddd7b12ea
Laurie Clark-Michalek <lclarkmichalek@gmail.com>
2015-04-25 12:35:16 +0100

    Added drop odd example (slows down the connection quite a lot, as it turns out)

--- examples/drop_odd.rs
+++ examples/drop_odd.rs
@@ -12,24 +12,32 @@ use nfq::verdict::Verdict;
 
 fn main() {
     let mut handle = Handle::new().ok().unwrap();
-    let mut queue = handle.queue(0, Decider).unwrap();
+    let mut queue = handle.queue(0, Decider{count: 0}).unwrap();
 
     handle.bind(ProtocolFamily::INET);
     queue.mode(CopyMode::Packet(4096)).ok();
 
-    println!("Listen for packets...");
+    println!("Listening for packets...");
     handle.start(4096);
 
     println!("Finished...");
 }
 
-struct Decider;
+struct Decider {
+    count: i32
+}
 
 impl VerdictHandler for Decider {
     fn decide(&mut self, message: &mut Message) -> Verdict {
         let id = message.header.id();
-        println!("Handling packet (ID: {})", id);
 
-        Verdict::Accept
+        self.count += 1;
+        if self.count % 2 == 0 {
+            println!("Accepting packet {}", id);
+            Verdict::Accept
+        } else {
+            println!("Dropping packet {}", id);
+            Verdict::Drop
+        }
     }
 }
